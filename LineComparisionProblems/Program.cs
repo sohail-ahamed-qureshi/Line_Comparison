1b4f9f627dbfde1f70a092a2d986e4376217fde5
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-02 19:45:59 +0530

    [sohail] Add: Welcome message

---
+++ LineComparisionProblems/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparisionProblems
+{
+    class Program
+    {/// <summary>
+    /// Welcomr message for Line Comparison problemstatements.
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Problem Statements");
+        }
+    }
+}
